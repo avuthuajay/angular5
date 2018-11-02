ffc0ecd4f23c3827f3edd0ba36d0033e4aeb5168
Ajay <avuthuajay@gmai.com>
2018-11-02 11:07:53 +0530

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ContentComponent } from './content/content.component';
+import { ShowpolicydetailsComponent } from './showpolicydetails/showpolicydetails.component';
+import { ShowAdvisorsComponent } from './show-advisors/show-advisors.component';
+import { ShowQuoteComponent } from './header/show-quote/show-quote.component';
+import { SearchBranchComponent } from './search-branch/search-branch.component';
+import { LogoutComponent } from './logout/logout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'home', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'products', component: ContentComponent },
+  { path: 'history', component: ShowpolicydetailsComponent },
+  { path: 'advisors', component: ShowAdvisorsComponent },
+  { path: 'quote', component: ShowQuoteComponent },
+  { path: 'branches', component: SearchBranchComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: '**', redirectTo: 'login' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -17,7 +17,10 @@ export class HeaderComponent implements OnInit {
     this.links = [
       {linkText:'Home', linkRef:'home', linkStyle:'nav-link'},
       {linkText:'Products', linkRef:'products', linkStyle:'nav-link'},
-      {linkText:'History', linkRef:'history', linkStyle:'nav-link'}
+      {linkText:'History', linkRef:'history', linkStyle:'nav-link'},
+      {linkText:'Advisors', linkRef:'advisors', linkStyle:'nav-link'},
+      {linkText:'Quote', linkRef:'quote', linkStyle:'nav-link'},
+      {linkText:'Branches', linkRef:'branches', linkStyle:'nav-link'}
     ];
     this.myHeading = 'Ajay Insurance';
     this.logo = './assets/images/logo.jpg';
